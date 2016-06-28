11018260a78690d9570000da858d932640e2a5ad
mboehme <hawkie@web.de>
2016-06-28 14:09:01 +0800

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Prints the list of co-authors of the PC members (pc.txt) from the last 5 years.
+
+LC_ALL=C
+LC_CTYPE=C
+LANG=C
+while read person; do
+  echo ""
+  urlpt=$(echo "$person" | sed 's/ /_/g' | sed 's/\.//g' | tr '[:upper:]' '[:lower:]')
+  echo $person : $urlpt
+  echo -----------------------
+  tmp=$(mktemp)
+  year=$(date +"%Y")
+  for year in $(seq $((year - 4)) $year); do
+    curl -s "http://dblp.org/search/publ/api?q=year%3A$year%3Aauthor%3A$urlpt&h=1000&format=xml" \
+    | xpath '/result/hits/hit/info/authors/author' 2>/dev/null >> $tmp
+  done
+  cat $tmp | sed 's/<\/author>/,/g' | tr , '\n' | sed 's/<author>//g' | sort | uniq
+done < pc.txt
